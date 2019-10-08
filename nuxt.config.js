1d460cd9088dedc5eb7704359862ac9fcb92a7ed
Justin Chiou <chiou.kai@gmail.com>
2019-10-08 13:47:28 -0400

    fix browser tab title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,25 @@
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "Riverfront Dancesport Festival",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
@@ -33,9 +33,9 @@ export default {
    */
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/pwa',
-    '@nuxtjs/eslint-module'
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/pwa",
+    "@nuxtjs/eslint-module"
   ],
   /*
    ** Build configuration
@@ -46,4 +46,4 @@ export default {
      */
     extend(config, ctx) {}
   }
-}
+};
