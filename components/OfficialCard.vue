619bfd02eb1f4295965e8a2f628767a00aba5840
Justin Chiou <chiou.kai@gmail.com>
2019-10-09 10:20:52 -0400

    parse out Official Card component

---
+++ components/OfficialCard.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  b-card
+    //- b-card-img(:src="require(`@/assets/officials/${official.imgSrc}`)" top)
+    b-card-text {{ official.name }}
+      |
+      |
+      small.text-muted ({{ official.role }})
+</template>
+
+<script>
+export default {
+  props: {
+    official: {
+      type: Object,
+      default() {},
+      imgSrc: {
+        type: String,
+        required: false
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      role: {
+        type: String,
+        required: true
+      }
+    }
+  }
+};
+</script>
--- pages/officials.vue
+++ pages/officials.vue
@@ -3,22 +3,21 @@
     b-container.py-5
       h1.text-center.pb-5 Officials
       b-card-group(columns)
-        b-card(
+        official-card(
           v-for="(official, index) in officials"
           :key="index"
-          :img-src="official.imgUrl"
-          img-top
+          :official="official"
         )
-          b-card-text {{ official.name }}
-            |
-            |
-            small.text-muted ({{ official.role }})
 </template>
 
 <script>
+import OfficialCard from "@/components/OfficialCard";
 import d from "@/components/data.json";
 
 export default {
+  components: {
+    OfficialCard
+  },
   data() {
     return {
       officials: d.officials
