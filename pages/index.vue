49aa4d2c2b0b1f22e72038891e9e7bfdaf44b218
Justin Chiou <chiou.kai@gmail.com>
2019-10-09 23:49:56 -0400

    clear commented components

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,14 @@
 <template lang="pug">
   main
     jumbotron
-    //- img(alt="Vue logo" src="@/assets/logo.png")
-    //- hello-world(msg="Welcome to Your Vue.js App")
-    //- officials-section
-    //- vendors-section
-    //- venue-section
 </template>
 
 <script>
 import Jumbotron from "@/components/Jumbotron";
-// import HelloWorld from "@/components/HelloWorld";
-// import OfficialsSection from "@/components/OfficialsSection";
-// import VendorsSection from "@/components/VendorsSection";
-// import VenueSection from "@/components/VenueSection";
 
 export default {
   components: {
     Jumbotron
-    // HelloWorld,
-    // OfficialsSection,
-    // VendorsSection,
-    // VenueSection
   }
 };
 </script>
