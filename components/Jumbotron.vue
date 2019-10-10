949d1182a5ea81d5c129df41c5fff8c8516fae53
Justin Chiou <chiou.kai@gmail.com>
2019-10-10 18:17:18 -0400

    use custom jumbotron classes

--- components/Jumbotron.vue
+++ components/Jumbotron.vue
@@ -1,14 +1,12 @@
 <template lang="pug">
-  b-jumbotron.bg-pic.text-center.mb-5(
-    text-variant="light"
-    bg-variant="transparent"
-    fluid
-  )
-    template(slot="header") Riverfront Dancesport Festival
-    template(slot="lead") Come celebrate our 10th year along the Riverfront!
-    hr.my-4.border-light
-    p.pb-5.text-left Riverfront Dancesport Festival is the best run and most friendly Dancesport competition in Greater Cincinnati. Regardless of your dancing level, we offer competition for all levels in all styles. Our 10th anniversary will be held on January 11, 2020, so don’t miss out!
-    b-button(variant="info" to="/forms" size="lg") Register!
+  .jumbotron.jumbotron-fluid.bg-pic.text-center.mb-5.text-light.bg-variant-transparent
+    b-container
+      h1.display-3.d-none.d-md-block Riverfront Dancesport Festival
+      h1.display-4.d-block.d-md-none Riverfront Dancesport Festival
+      p.lead Come celebrate our 10th year along the Riverfront!
+      hr.my-4.border-light
+      p.pb-5.text-left Riverfront Dancesport Festival is the best run and most friendly Dancesport competition in Greater Cincinnati. Regardless of your dancing level, we offer competition for all levels in all styles. Our 10th anniversary will be held on January 11, 2020, so don’t miss out!
+      b-button(variant="info" to="/forms" size="lg") Register!
 </template>
 
 <style scoped>
