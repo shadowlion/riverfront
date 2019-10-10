610fef900ae3bf917d9e057544e4a1a6b94d1c06
Justin Chiou <chiou.kai@gmail.com>
2019-10-10 00:07:36 -0400

    add section for showcasing Pro show

---
+++ components/ShowcaseContent.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  section
+    b-container.py-5
+      h1.pb-5.text-center This Year's Professional Show:
+      b-row
+        b-col.py-2(md="3" sm="12")
+          b-img(
+            thumbnail
+            fluid
+            src="~assets/showcase/kyle_allie_1.jpg"
+          )
+        b-col.py-2(md="3" sm="12")
+          b-img(
+            thumbnail
+            fluid
+            src="~assets/showcase/kyle_allie_2.jpg"
+          )
+        b-col.py-2(md="3" sm="12")
+          b-img(
+            thumbnail
+            fluid
+            src="~assets/showcase/kyle_allie_3.jpg"
+          )
+        b-col.py-2(md="3" sm="12")
+          b-img(
+            thumbnail
+            fluid
+            src="~assets/showcase/kyle_allie_4.jpg"
+          )
+      b-row
+        b-col
+          p.py-4 This year’s Riverfront Professional Show will feature the exciting American Style Smooth dance couple, Kyle and Allie Spinder.  So please plan now to attend our fantastic competition on January 11, 2020 and see these great performers at the 2020 Riverfront Dancesport Festival. You’ll be glad you did!
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
   main
     jumbotron
+    showcase-content
 </template>
 
 <script>
 import Jumbotron from "@/components/Jumbotron";
+import ShowcaseContent from "@/components/ShowcaseContent";
 
 export default {
   components: {
-    Jumbotron
+    Jumbotron,
+    ShowcaseContent
   }
 };
 </script>
