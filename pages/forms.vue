35e0508650f8672c88afc162570e4ddf2a1e1ae1
Justin Chiou <chiou.kai@gmail.com>
2019-10-14 11:17:56 -0400

    add 2020 files as static assets

--- pages/forms.vue
+++ pages/forms.vue
@@ -36,13 +36,17 @@
         | for the password.
       b-table-lite(striped hover :fields="fields" :items="items" caption-top)
         template(#table-caption) Look below to view or download the forms you need to register:
-        template(#cell(download_link)="data")
-          a(:href="data.item.download" _target="blank" download)
+        template(#cell(download)="data")
+          a(
+            :href="`/docs/2020/2020RF${data.item.download}.pdf`"
+            _target="blank"
+          )
             download-cloud-icon
 </template>
 
 <script>
 import { DownloadCloudIcon } from "vue-feather-icons";
+import d from "@/components/data.json";
 
 export default {
   components: {
@@ -50,34 +54,8 @@ export default {
   },
   data() {
     return {
-      fields: [
-        "file_name",
-        "notes",
-        { key: "download_link", class: "text-center" }
-      ],
-      items: [
-        {
-          file_name: "Waiver/Release Form",
-          notes: "Early Bird deadline 12/31/2019",
-          download:
-            "http://riverfrontdancesportfestival.com/downloads/2019RFWaiverReleaseForm.pdf"
-        },
-        {
-          file_name: "General Admission Form",
-          download:
-            "http://riverfrontdancesportfestival.com/downloads/2019RFWaiverReleaseForm.pdf"
-        },
-        {
-          file_name: "Spectator/Admission Form",
-          download:
-            "http://riverfrontdancesportfestival.com/downloads/2019RFWaiverReleaseForm.pdf"
-        },
-        {
-          file_name: "Summary Form",
-          download:
-            "http://riverfrontdancesportfestival.com/downloads/2019RFWaiverReleaseForm.pdf"
-        }
-      ]
+      fields: ["file_name", "notes", { key: "download", class: "text-center" }],
+      items: d.docs
     };
   }
 };
