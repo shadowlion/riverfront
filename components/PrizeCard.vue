acdeaa9f9982c0e15bed97a21ec1556951445492
Justin Chiou <chiou.kai@gmail.com>
2019-10-16 11:29:31 -0400

    create a prize card with conditional props

---
+++ components/PrizeCard.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  b-card(no-body)
+    template(#header) {{ prize.title }}
+    b-list-group(flush)
+      b-list-group-item(
+        v-for="(item, index) in prize.list"
+        :key="index"
+      ) {{ index + 1 }}: {{ item }}
+    b-card-body(v-if="prize.text")
+      b-card-text {{ prize.text }}
+</template>
+
+<script>
+export default {
+  props: {
+    prize: {
+      type: Object,
+      default() {},
+      title: {
+        type: String,
+        required: true
+      },
+      list: {
+        type: Array,
+        default() {},
+        required: false
+      },
+      text: {
+        type: String,
+        required: false
+      }
+    }
+  }
+};
+</script>
--- pages/prizes.vue
+++ pages/prizes.vue
@@ -2,4 +2,26 @@
   main
     b-container.py-5
       h1.text-center.pb-5 Prizes
+      b-card-group(columns)
+        prize-card(
+          v-for="(prize, index) in prizes"
+          :key="index"
+          :prize="prize"
+        )
 </template>
+
+<script>
+import d from "@/components/data.json";
+import PrizeCard from "@/components/PrizeCard";
+
+export default {
+  components: {
+    PrizeCard
+  },
+  data() {
+    return {
+      prizes: d.prizes
+    };
+  }
+};
+</script>
